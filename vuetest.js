bb87dbc2833c1ee1f5f905b033f5b3024474336e
caiweirong11 <108159031+caiweirong11@users.noreply.github.com>
2022-10-27 11:05:21 +0800

    github上传tapd test
    --story=1000194 --user=蔡伟荣 前端 https://www.tapd.cn/39390025/s/1000054

---
+++ vuetest.js
@@ -0,0 +1,17 @@
+
+    <div id="app">
+        <input type="button" value="v-on指令" v-on:click="doIt">
+        <input type="button" value="v-on简写" @click="doIt">
+        <input type="button" value="双击事件" @dblclick="doIt">
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script>
+        var app = new Vue({
+            el: "#app",
+            methods: {
+                doIt: function () {
+                    alert("Do It");
+                }
+            },
+        })
